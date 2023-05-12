1685b665f39c660936ae706ba4805befb5fc4425
GroellerKarim <karim.jonas.groeller@gmail.com>
2023-05-12 13:02:47 +0200

    Started Memento-Pattern Demo

---
+++ memento_demo/src/main/java/eu/groeller/memento_demo/MementoDemoApplication.java
@@ -0,0 +1,13 @@
+package eu.groeller.memento_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MementoDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MementoDemoApplication.class, args);
+    }
+
+}
